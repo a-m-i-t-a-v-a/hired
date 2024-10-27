fa1b9e7b1314fd5cd57b257ddcea67b6885a93dd
a-m-i-t-a-v-a <140612284+a-m-i-t-a-v-a@users.noreply.github.com>
2024-10-27 22:40:28 +0530

    First

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Button variant="outline">Click me</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,jsx}", // Includes JS and JSX files within src
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
+
+
